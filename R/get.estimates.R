9eca7f6917c32f9556eb3968b03aa7d9f80807e8
Vadim Zyamalov <zyamalov.ve@gmail.com>
2023-07-06 21:25:13 +0300

    Some corrections in get.estimates.R

--- R/get.estimates.R
+++ R/get.estimates.R
@@ -11,12 +11,11 @@ get.estimates <- function(est, model, g.function) {
     vec.E  <- vec(t(model$data$Y)) - vec.Yf
 
     B  <- matrix(est$vec.B, ncol = k * m)
-    rownames(B) <- colnames(model$data$X)
     colnames(B) <- paste("R",
                          rep(1:m, each = k),
                          ".",
                          colnames(model$data$Y), sep = "")
-
+    rownames(B) <- colnames(model$data$X)
 
     Yf <- t(matrix(vec.Yf, ncol = N, byrow = FALSE))
     colnames(Yf) <- paste("fit.", colnames(model$data$Y), sep = "")
@@ -36,6 +35,11 @@ get.estimates <- function(est, model, g.function) {
     GX <- cbind(model$data$X,
                 (t(unity(ncol(model$data$X))) %x% G.mat) * model$data$X)
     VAR <- matrix(diag(OMEGA %x% MASS::ginv(t(GX) %*% GX)), ncol = k * m)
+    colnames(VAR) <- paste("R",
+                           rep(1:m, each = k),
+                           ".",
+                           colnames(model$data$Y), sep = "")
+    rownames(VAR) <- colnames(model$data$X)
 
     return(list(coef = B,
                 sd = sqrt(VAR),
