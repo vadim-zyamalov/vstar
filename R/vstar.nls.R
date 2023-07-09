394c0abcb3c89f474d2fcd903843a5ee81866807
Vadim Zyamalov <zyamalov.ve@gmail.com>
2023-07-09 20:28:22 +0300

    NLS function
    Well, I moved from built-in nls() function to the straightforward NLS
    implementation. I've just directly minimize sum of squares. But it still
    converges in 2 steps...

--- R/vstar.nls.R
+++ R/vstar.nls.R
@@ -1,18 +1,28 @@
 #' @importFrom stats nls
 #' @importFrom stats nls.control
 #' @importFrom matrixcalc vec
+#' @importFrom optimx optimx
 #'
 #' @export
 vstar.nls <- function(model,
                       tol = 1e-6,
                       iter = 10000,
+                      algorithm = "default",
                       control = nls.control()) {
     vec.y <- vec(t(model$data$Y))
     G.func <- get.G.function(model$g.function)
 
-    yf <- function(g, thr, vec.B) { # nolint
+    yf <- function(g, thr) { # nolint
         BM <- get.B.mat(model, g, thr, G.func)
         BM$M %*% BM$vec.B
+    }
+
+    Q <- function(gc) {
+        g <- gc[1:(model$dim$m - 1)]
+        thr <- gc[(model$dim$m):length(gc)]
+        BM <- get.B.mat(model, g, thr, G.func)
+        vec.E <- vec(t(model$data$Y)) - BM$M %*% BM$vec.B
+        drop(t(vec.E) %*% vec.E)
     }
 
     iter.g <- model$estimates$g
@@ -22,15 +32,23 @@ vstar.nls <- function(model,
     converged <- FALSE
 
     for (step in 1:iter) {
-        iter.nls <- nls(vec.y ~ yf(gx, cx, iter.B),
-                        start = list(gx = iter.g, cx = iter.c),
-                        control = control)
+        cat(step, "\n")
+        #iter.nls <- nls(vec.y ~ yf(gx, cx),
+        #                start = list(gx = iter.g, cx = iter.c),
+        #                algorithm = algorithm,
+        #                control = control)
 
-        result <- coef(iter.nls)
+        #result <- coef(iter.nls)
+        iter.nls <- optim(c(iter.g, iter.c), Q)
+        result <- iter.nls$par
+
         dd <- result - c(iter.g, iter.c)
 
-        iter.g <- result[startsWith(names(result), "gx")]
-        iter.c <- result[startsWith(names(result), "cx")]
+        #iter.g <- result[startsWith(names(result), "gx")]
+        iter.g <- result[1:(model$dim$m - 1)]
+        #iter.c <- result[startsWith(names(result), "cx")]
+        iter.c <- result[(model$dim$m):length(result)]
+
         iter.BM <- get.B.mat(model, iter.g, iter.c, G.func)
         iter.B <- iter.BM$vec.B
 
@@ -46,9 +64,9 @@ vstar.nls <- function(model,
         cat("Possible absence of convergence!\n")
     }
 
-    nls.summary <- summary(iter.nls)
-    g.sd <- nls.summary$coefficients[, 2][names(iter.g)]
-    c.sd <- nls.summary$coefficients[, 2][names(iter.c)]
+    #nls.summary <- summary(iter.nls)
+    #g.sd <- nls.summary$coefficients[, 2][names(iter.g)]
+    #c.sd <- nls.summary$coefficients[, 2][names(iter.c)]
 
     nls.result <- list(vec.B = iter.BM$vec.B,
                        vec.E = vec.y - iter.BM$M %*% iter.BM$vec.B,
@@ -56,9 +74,9 @@ vstar.nls <- function(model,
                        SSR = drop(t(model$estimates$vec.E) %*%
                                 model$estimates$vec.E),
                        g = iter.g,
-                       g.sd = g.sd,
-                       c = iter.c,
-                       c.sd = c.sd)
+                       #g.sd = g.sd,
+                       c = iter.c)
+                       #c.sd = c.sd)
 
     final.est <- get.estimates(nls.result, model, model$g.function)
 
@@ -66,9 +84,9 @@ vstar.nls <- function(model,
                    sd = final.est$sd,
                    t.stat = final.est$t.stat,
                    g = iter.g,
-                   g.sd = g.sd,
+                   #g.sd = g.sd,
                    c = iter.c,
-                   c.sd = c.sd,
+                   #c.sd = c.sd,
                    fitted.values = final.est$fitted.values,
                    residuals = final.est$residuals,
                    cov = final.est$cov,
