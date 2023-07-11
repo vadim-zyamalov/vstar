86ce450b383b29ea2e8b8ef305d908010dcba13c
Vadim Zyamalov <zyamalov.ve@gmail.com>
2023-07-11 10:18:27 +0300

    Fixes for correct multiple regimes handling
    And more. Git gathered all changes into one chunk.
    
    Corrected was also a code for coefficients SD calculations.
    Check of the input object class was added, we should have either
    `vstar.data` or `vstar` class object.

--- R/get.estimates.R
+++ R/get.estimates.R
@@ -1,45 +1,53 @@
 #' @importFrom MASS ginv
 #' @importFrom matrixcalc vec
-get.estimates <- function(est, model, g.function) {
-    k <- model$dim$k
+get.estimates <- function(est, dataset, g.function) {
+    if (!any(c("vstar", "vstar.data") %in% class(dataset))) {
+        stop("Wrong `model`: an object with prepared data or estimated model is needed!")
+    }
+
+    k <- dataset$dim$k
     m <- est$m
-    N <- model$dim$N
+    N <- dataset$dim$N
+    Nx <- ncol(dataset$data$X)
 
     G.func <- get.G.function(g.function)
 
     vec.Yf <- est$M %*% est$vec.B
-    vec.E  <- vec(t(model$data$Y)) - vec.Yf
+    vec.E  <- vec(t(dataset$data$Y)) - vec.Yf
 
     B  <- matrix(est$vec.B, ncol = k * m)
     colnames(B) <- paste("R",
                          rep(1:m, each = k),
                          ".",
-                         colnames(model$data$Y), sep = "")
-    rownames(B) <- colnames(model$data$X)
+                         colnames(dataset$data$Y), sep = "")
+    rownames(B) <- colnames(dataset$data$X)
 
     Yf <- t(matrix(vec.Yf, ncol = N, byrow = FALSE))
-    colnames(Yf) <- paste("fit.", colnames(model$data$Y), sep = "")
-    rownames(Yf) <- rownames(model$data$Y)
+    colnames(Yf) <- paste("fit.", colnames(dataset$data$Y), sep = "")
+    rownames(Yf) <- rownames(dataset$data$Y)
 
     E <- t(matrix(vec.E, ncol = N, byrow = FALSE))
-    colnames(E) <- paste("resid.", colnames(model$data$Y), sep = "")
-    rownames(E) <- rownames(model$data$Y)
+    colnames(E) <- paste("resid.", colnames(dataset$data$Y), sep = "")
+    rownames(E) <- rownames(dataset$data$Y)
 
     OMEGA <- (1 / N) * t(E) %*% E
 
-    gg <- unity(N) %x% t(est$g)
-    cc <- unity(N) %x% t(est$c)
+    sm <- dataset$data$S %x% t(unity(m - 1))
+    gm <- unity(N) %x% t(est$g)
+    cm <- unity(N) %x% t(est$c)
 
-    G.mat <- G.func(model$data$S, gg, cc)
+    G.mat <- G.func(sm, gm, cm)
+    G.mat <- cbind(1, G.mat)
 
-    GX <- cbind(model$data$X,
-                (t(unity(ncol(model$data$X))) %x% G.mat) * model$data$X)
-    VAR <- matrix(diag(OMEGA %x% MASS::ginv(t(GX) %*% GX)), ncol = k * m)
+    GX <- (t(unity(Nx)) %x% G.mat) * (dataset$data$X %x% t(unity(m)))
+    VAR <- matrix(diag(ginv(t(GX) %*% GX) %x% OMEGA),
+                  ncol = k * m,
+                  byrow = TRUE)
     colnames(VAR) <- paste("R",
                            rep(1:m, each = k),
                            ".",
-                           colnames(model$data$Y), sep = "")
-    rownames(VAR) <- colnames(model$data$X)
+                           colnames(dataset$data$Y), sep = "")
+    rownames(VAR) <- colnames(dataset$data$X)
 
     return(list(coef = B,
                 sd = sqrt(VAR),
