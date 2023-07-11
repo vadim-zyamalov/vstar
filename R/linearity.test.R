f38f144b46a0d9ac68e9c53bb4adaee05b474482
Vadim Zyamalov <zyamalov.ve@gmail.com>
2023-07-11 10:27:52 +0300

    Size corrected statistics added
    - Rescaled LM statistic by Laitinen and Maisner.
    - Rao's F-statistic.
    - Bartlett's approximation of Wilks's Lambda-statistic.

--- R/linearity.test.R
+++ R/linearity.test.R
@@ -33,13 +33,76 @@ linearity.test <- function(dataset, J = 1, stat.type = "all") {
     En <- t(matrix(vEn, ncol = N))
     SSRn <- t(En) %*% En
 
-    LM.stat <- N * (k - matrix.trace(ginv(SSR0) %*% SSRn))
+    result <- NULL
 
-    result <- list(LM.stat = LM.stat,
-                   crit.10 = qchisq(.90, df = J * k * Nx),
-                   crit.5  = qchisq(.95, df = J * k * Nx),
-                   crit.1  = qchisq(.99, df = J * k * Nx),
-                   p.value = 1 - pchisq(LM.stat, df = J * k * Nx))
+    if (stat.type %in% c("all", "LM")) {
+        LM.stat <- N * (k - matrix.trace(ginv(SSR0) %*% SSRn))
+        .df <- J * k * Nx
+
+        result <- rbind(
+            result,
+            "LM" = c(stat    = LM.stat,
+                     crit.10 = qchisq(.90, df = .df),
+                     crit.5  = qchisq(.95, df = .df),
+                     crit.1  = qchisq(.99, df = .df),
+                     p.value = 1 - pchisq(LM.stat, df = .df))
+        )
+    }
+
+    if (stat.type %in% c("all", "r-LM")) {
+        LM.stat <- N * (k - matrix.trace(ginv(SSR0) %*% SSRn))
+
+        .K <- (J + 1) * k * Nx
+        .G <- J * k * Nx
+
+        F.stat <- LM.stat * (k * N - .K) / (.G * k * N)
+
+        .df1 <- .G
+        .df2 <- k * N - .K
+
+        result <- rbind(
+            result,
+            "r-LM" = c(stat    = F.stat,
+                       crit.10 = qf(.90, df1 = .df1, df2 = .df2),
+                       crit.5  = qf(.95, df1 = .df1, df2 = .df2),
+                       crit.1  = qf(.99, df1 = .df1, df2 = .df2),
+                       p.value = 1 - pf(F.stat, df1 = .df1, df2 = .df2))
+        )
+    }
+
+    if (stat.type %in% c("all", "Rao")) {
+        .s <- sqrt((ncol(Zn)^2 * k^2 - 4) / (k^2 + ncol(Zn)^2 - 5))
+        .N <- N - ncol(dataset$data$X) - .5 * (k + ncol(Zn) + 1)
+
+        .df1 <- ncol(Zn) * k
+        .df2 <- .N * .s - .5 * ncol(Zn) * k + 1
+
+        F.stat <- ((det(SSR0) / det(SSRn))^(1/.s) - 1) * (.df2 / .df1)
+
+        result <- rbind(
+            result,
+            "Rao" = c(stat    = F.stat,
+                      crit.10 = qf(.90, df1 = .df1, df2 = .df2),
+                      crit.5  = qf(.95, df1 = .df1, df2 = .df2),
+                      crit.1  = qf(.99, df1 = .df1, df2 = .df2),
+                      p.value = 1 - pf(F.stat, df1 = .df1, df2 = .df2))
+        )
+    }
+
+    if (stat.type %in% c("all", "Bartlett-Wilks")) {
+        l.stat <- (.5 * (k + ncol(Zn) + 1) + ncol(dataset$data$X) - N) *
+            log(det(SSRn) / det(SSR0))
+        .df <- ncol(Zn) * k
+
+        result <- rbind(
+            result,
+            "Bartlett-Wilks" = c(stat    = l.stat,
+                                 crit.10 = qchisq(.90, df = .df),
+                                 crit.5  = qchisq(.95, df = .df),
+                                 crit.1  = qchisq(.99, df = .df),
+                                 p.value = 1 - pchisq(l.stat, df = .df))
+        )
+    }
 
     return(result)
 }
